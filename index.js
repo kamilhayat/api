ddaf1f22bec69b346135f2a6694340acdc146d72
Kamil Hayat <kamilhayat27@gmail.com>
2024-11-26 21:40:45 +0530

    final commit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const mongoose = require('mongoose')
 const port = process.env.PORT || 5000
-const productRoutes= require('./src/routes/index')
+const productRoutes = require('./src/routes/index')
 require('dotenv').config();
 
 
 const app = express()
 app.use(express.json());
-mongoose.connect(process.env. MONGODB_URI,{})
-.then(()=>{
-    console.log("Connected to MongoDB")
-})
-.catch((err)=>{
-    console.log("Error connecting to MongoDB", err)
-})
+mongoose.connect(process.env.MONGODB_URI, {})
+    .then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB", err)
+    })
 
 app.use('/api/products', productRoutes);
 
-
-
-
+app.get('/', (req, res) => {
+    res.send('API is working!');  // Simple message to confirm the API is up
+});
 
 app.listen(port, () => {
 
