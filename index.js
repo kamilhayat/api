3eb2ae252d5dc6a6141fb8eae0a3b8285e31eb98
Kamil Hayat <kamilhayat27@gmail.com>
2024-11-27 17:52:09 +0530

    final chnages

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const mongoose = require('mongoose')
-const port = process.env.PORT || 8000
-const productRoutes = require('./src/routes/index')
+const mongoose = require('mongoose');
+const cors = require('cors'); 
+const port = process.env.PORT || 8000;
+const productRoutes = require('./src/routes/index');
 require('dotenv').config();
 
+const app = express();
 
-const app = express()
+app.use(cors());  
+
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {})
     .then(() => {
-        console.log("Connected to MongoDB")
+        console.log("Connected to MongoDB");
     })
     .catch((err) => {
-        console.log("Error connecting to MongoDB", err)
-    })
+        console.log("Error connecting to MongoDB", err);
+    });
 
 app.use('/api/products', productRoutes);
 
@@ -22,7 +25,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-
     console.log(`Connected to Port no. ${port}`);
-})
-
+});
